bbb826799631aa08e43b0957cf2b05ce3ae7ce20
ribizli <ribizli@users.noreply.github.com>
2022-04-16 08:29:30 +0200

    deps upgrade

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
-export { concat, equals } from 'https://deno.land/std@0.92.0/bytes/mod.ts';
+export { concat, equals } from 'https://deno.land/std@0.135.0/bytes/mod.ts';
+export { iterateReader } from 'https://deno.land/std@0.135.0/streams/conversion.ts';
